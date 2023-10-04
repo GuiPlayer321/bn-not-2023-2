359af6c49441c6792a574b0976b8acc21e17479a
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-04 20:07:05 -0300

    Termino do basico do back-end

--- back-end/src/controllers/aluno.js
+++ back-end/src/controllers/aluno.js
@@ -1,67 +1,201 @@
-import prisma from "../database/client.js";
+import prisma from '../database/client.js'
 
-const controller = {};
+const controller = {}   // Objeto vazio
 
-controller.create = async function (req, res) {
+controller.create = async function(req, res) {
   try {
-    await prisma.aluno.create({ data: req.body });
-    res.status(201).end();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
+
+    // Conecta-se ao BD e envia uma instrução
+    // de criação de um novo documento, com os
+    // dados que estão dentro de req.body
+    await prisma.aluno.create({data: req.body})
+
+    // Envia uma resposta de sucesso ao front-end
+    // HTTP 201: Created
+    res.status(201).end()
   }
-};
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
 
-controller.retrieveAll = async function (req, res) {
+controller.retrieveAll = async function(req, res) {
+
+  // Por padrão, não inclui nenhum relacionamento
+  const include = {}
+
+  if(req.query.turmas)   include.turmas = true
+
   try {
+    // Manda buscar os dados no servidor
     const result = await prisma.aluno.findMany({
-      orderBy: [{ nome: "asc" }],
-    });
-    res.send(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
+      include,
+      orderBy: [
+        { nome: 'asc' }  // Ordem ascendente
+      ]
+    })
+    // HTTP 200: OK
+    res.send(result)
   }
-};
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
 
-controller.retrieveOne = async function (req, res) {
+controller.retrieveOne = async function(req, res) {
   try {
     const result = await prisma.aluno.findUnique({
-      where: { id: req.params.id },
-    });
-    if (result) res.send(result);
-    else res.status(404).end();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
-};
+      where: { id: req.params.id }
+    })
 
-controller.update = async function (req, res) {
+    // Encontrou ~> retorna HTTP 200: OK
+    if(result) res.send(result)
+    // Não encontrou ~> retorna HTTP 404: Not found
+    else res.status(404).end()
+  }
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.update = async function(req, res) {
+
   try {
     const result = await prisma.aluno.update({
       where: { id: req.params.id },
-      data: req.body,
-    });
-    if (result) res.status(204).end();
-    else res.status(404).end();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
-};
+      data: req.body
+    })
 
-controller.delete = async function (req, res) {
+    // Encontrou e atualizou ~> retorna HTTP 204: No content
+    if(result) res.status(204).end()
+    // Não encontrou (e não atualizou) ~> retorna HTTP 404: Not found
+    else res.status(404).end()
+  }
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.delete = async function(req, res) {
   try {
     const result = await prisma.aluno.delete({
-      where: { id: req.params.id },
-    });
-    if (result) res.status(204).end();
-    else res.status(404).end();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
-};
+      where: { id: req.params.id }
+    })
 
-export default controller;
+    // Encontrou e excluiu ~> retorna HTTP 204: No content
+    if(result) res.status(204).end()
+    // Não encontrou (e não excluiu) ~> retorna HTTP 404: Not found
+    else res.status(404).end()
+  }
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.addTurma = async function(req, res) {
+  try {
+    
+    // Busca o aluno para recuperar a lista de ids de turmas dele
+    const aluno = await prisma.aluno.findUnique({
+      where: { id: req.params.alunoId }
+    })
+
+    // Se ele não tiver turmas ainda, criamos a lista vazia
+    const turmaIds = aluno.turmaIds || []
+
+    // Se o id de turma passado ainda não estiver na lista do
+    // aluno, fazemos a respectiva inserção
+    if(! turmaIds.includes(req.params.turmaId))
+      turmaIds.push(req.params.turmaId)
+
+    // Atualizamos o aluno com uma lista de ids de turma atualizada  
+    const result = await prisma.aluno.update({
+      where: { id: req.params.alunoId },
+      data: { turmaIds }
+    })
+
+    // Encontrou e atualizou ~> retorna HTTP 204: No content
+    if(result) res.status(204).end()
+    // Não encontrou (e não atualizou) ~> retorna HTTP 404: Not found
+    else res.status(404).end()
+
+  }
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+
+controller.removeTurma = async function(req, res) {
+  try {
+
+    // Busca o aluno para recuperar a lista de ids de turmas dele
+    const aluno = await prisma.aluno.findUnique({
+      where: { id: req.params.alunoId }
+    })
+
+    // Não encontrou o aluno, ou o aluno não tem turmas
+    // associadas a ele ~> HTTP 404: Not Found
+    if(! aluno || ! aluno.turmaIds) res.send(404).end()
+
+    // Procura, na lista de ids de turma do aluno, se existe o id
+    // de turma passado para remoção
+    for(let i = 0; i < aluno.turmaIds.length; i++) {
+      // Encontrou
+      if(aluno.turmaIds[i] === req.params.turmaId) {
+        // Remove o id que foi passado da lista de ids de turma
+        aluno.turmaIds.splice(i, 1)
+
+        // Faz a atualização no aluno, alterando o conteúdo de turmasId
+        const result = await prisma.aluno.update({
+          where: { id: req.params.alunoId },
+          data: { turmaIds: aluno.turmaIds }
+        })
+
+        // Encontrou e atualizou ~> retorna HTTP 204: No content
+        if(result) return res.status(204).end()
+        // Não encontrou (e não atualizou) ~> retorna HTTP 404: Not found
+        else return res.status(404).end()
+      
+      }
+    }
+
+    // Se chegou até aqui, é porque não existe o id da turma passado
+    // na lista de ids de turma do aluno ~> HTTP 404: Not found
+    return res.status(404).end()    
+
+  }
+  catch(error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error)
+    // Envia o erro ao front-end, com status 500
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+export default controller
